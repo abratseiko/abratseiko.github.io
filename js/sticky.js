b33a10d9020e2a655c1bd60c7b2cff34ee3e5e11
abratseiko <abr.softevol@gmail.com>
2016-05-31 13:34:48 +0300

    f

--- js/sticky.js
+++ js/sticky.js
@@ -49,10 +49,10 @@
 
 			var currentItem = getCurrentElemPosition(elem);
 
-			if (differentOfHeight < 0 && differentOfWidth == 0){
-            	newStyles.top = differentOfHeight + 'px';
-            	// container.style.top = differentOfHeight + 'px';
-            }
+			// if (differentOfHeight < 0 && differentOfWidth == 0){
+   //          	newStyles.top = differentOfHeight + 'px';
+   //          	// container.style.top = differentOfHeight + 'px';
+   //          }
 	
 			if( differentOfWidth > 0 ){
 				if( currentItem.bottom <= container.offsetTop ){
@@ -63,7 +63,7 @@
 				}
 			} else {
 				if( currentItem.top <= container.offsetTop ){
-					elem.style.position = "sticky";
+					// elem.style.position = "sticky";
 					setNewStyles(newStyles);
 				} else {
 					setStartStyles();
